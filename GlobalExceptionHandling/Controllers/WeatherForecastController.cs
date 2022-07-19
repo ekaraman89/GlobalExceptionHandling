8481bdeb2ca9e71e904264282d952aa82fb1867d
emrah karaman <ekaraman89@hotmail.com>
2022-07-19 22:10:43 +0300

    Add project files.

---
+++ GlobalExceptionHandling/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalExceptionHandling.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IActionResult Get()
+        {
+            IEnumerable<WeatherForecast> list = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+              .ToArray();
+
+            if (list != null)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return Ok(list);
+        }
+    }
+}
---
+++ GlobalExceptionHandling/Middleware/GlobalExceptionHandlingMiddleware.cs
@@ -0,0 +1,63 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace GlobalExceptionHandling.Middleware
+{
+    public class GlobalExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            HttpResponse response = context.Response;
+            ResponseModel exModel = new ResponseModel();
+
+            switch (exception)
+            {
+                case ApplicationException ex:
+                    exModel.responseCode = (int)HttpStatusCode.BadRequest;
+                    response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    exModel.responseMessage = "Bir hata oluştu, Lütfen bir süre sonra tekrar deneyin.";
+                    break;
+
+                case FileNotFoundException ex:
+                    exModel.responseCode = (int)HttpStatusCode.NotFound;
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    exModel.responseMessage = "Dosya bulunamadı";
+                    break;
+
+                case IndexOutOfRangeException ex:
+                    exModel.responseCode = (int)HttpStatusCode.NotFound;
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    exModel.responseMessage = "Dizi veya koleksiyonun sınırlarının dışındadır.";
+                    break;
+
+                default:
+                    exModel.responseCode = (int)HttpStatusCode.InternalServerError;
+                    response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    exModel.responseMessage = "Internal Server Error, Lütfen bir süre sonra tekrar deneyin.";
+                    break;
+            }
+            string exResult = JsonSerializer.Serialize(exModel);
+            await context.Response.WriteAsync(exResult);
+        }
+    }
+}
